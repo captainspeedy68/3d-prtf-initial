4756534d06e2d0d809a4770f45bf9946070cef87
Bishal Acharya <82877265+captainspeedy68@users.noreply.github.com>
2025-04-10 18:49:18 +0600

    initial commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+const Home = () => {
+    return (
+        <div>
+            Hello this is the home component
+        </div>
+    );
+};
+
+export default Home;
